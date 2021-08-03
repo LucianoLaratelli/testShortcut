766c3ddb46b796720dee7e3f024df00d642f9359
Luciano Laratelli <luciano@laratel.li>
2021-08-03 18:06:08 -0400

    implement handler methods for intent extension
    note that this doesn't actually run -- I can launch the intents
    extension from xcode (using either Siri or Shortcuts.app) and it'll
    build and run, but the Shortcuts app can't find the shortcut.

--- intents-extension/IntentHandler.m
+++ intents-extension/IntentHandler.m
@@ -6,8 +6,9 @@
 //
 
 #import "IntentHandler.h"
+#import "CreateTaskIntent.h"
 
-@interface IntentHandler ()
+@interface IntentHandler () <CreateTaskIntentHandling>
 
 @end
 
@@ -16,8 +17,39 @@
 - (id)handlerForIntent:(INIntent *)intent {
     // This is the default implementation.  If you want different objects to handle different intents,
     // you can override this and return the handler you want for that particular intent.
-    
     return self;
+}
+
+- (void)handleCreateTask:(nonnull CreateTaskIntent *)intent completion:(nonnull void (^)(CreateTaskIntentResponse * _Nonnull))completion {
+    CreateTaskIntentResponse *response =[[CreateTaskIntentResponse alloc] initWithCode:CreateTaskIntentResponseCodeSuccess userActivity:nil];
+    Task *task = [[Task alloc] initWithIdentifier:[intent Title] displayString:[intent Title]];
+    [task setDueDate:[intent dueDate]];
+    [task setTitle:[intent Title]];
+    
+    [response setTask:task];
+    
+    NSLog(@"created task with title %@ and due date %@", [intent Title], [[intent dueDate] date]);
+    
+    completion(response);
+}
+
+- (void)resolveDueDateForCreateTask:(nonnull CreateTaskIntent *)intent withCompletion:(nonnull void (^)(CreateTaskDueDateResolutionResult * _Nonnull))completion {
+    NSDateComponents * dueDateComponents = [intent dueDate];
+    if (dueDateComponents == nil) {
+        return completion([CreateTaskDueDateResolutionResult needsValue]);
+    }
+    if([[dueDateComponents date] compare: [NSDate date]] == NSOrderedAscending){
+        return completion([CreateTaskDueDateResolutionResult unsupportedWithReason:CreateTaskDueDateUnsupportedReasonInvalidDate]);
+    }
+    
+    return completion([CreateTaskDueDateResolutionResult successWithResolvedDateComponents:[intent dueDate]]);
+}
+
+- (void)resolveTitleForCreateTask:(nonnull CreateTaskIntent *)intent withCompletion:(nonnull void (^)(INStringResolutionResult * _Nonnull))completion {
+    if ([intent Title] == nil){
+        return completion([INStringResolutionResult needsValue]);
+    }
+    return completion([INStringResolutionResult successWithResolvedString:[intent Title]]);
 }
 
 @end
